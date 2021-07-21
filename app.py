522ee183d4b4e65a6743cb268f2bf0e62d170461
Mahi Rahman <starexify@gmail.com>
2021-07-21 17:41:50 +1000

    Main page completed

--- app.py
+++ app.py
@@ -3,26 +3,41 @@ from pyhtml import *
 import requests
 import src.config as config
 
+headers = {
+    'x-rapidapi-key': config.API_KEY,
+    'x-rapidapi-host': "imdb8.p.rapidapi.com"
+    }
+
 app = Flask(__name__, static_url_path = '/public', static_folder = 'public')
-app_title = "Movie Content Advisory Directory"
+app_title = config.TITLE
+
 @app.route('/')
 def main():
-    content = html(
+    main_page = html(
         head(
+            meta(charset="utf-8"),
+            meta(name="viewport", content="width=device-width, initial-scale=1, shrink-to-fit=no"),
             title(app_title),
+            link(rel="stylesheet", href="https://use.fontawesome.com/releases/v5.15.2/css/all.css"),
+            link(rel="stylesheet", href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"),
+            link(rel="stylesheet", href="public/css/mdb.min.css"),
             link(rel="stylesheet", href="public/css/style.css"),
-            link(rel="icon", href="public/fav/favicon.ico")
+            link(rel="icon", href= config.FAVICON),
+            script(src="public/js/mdb.min.js")
         ),
         body(
-            p("Movie Content Advisory Directory"),
+            img(src=config.LOGO, alt="MCAD Logo", class_ ="logo center selector"),
+
             form(action="movies")(
-                label("Search"),
-                input_(type="text", name="movie_title"),
-                input_(type="Submit", value="Search")
+                input_(type="text", name="movie_title", placeholder="Search", class_="searchbox"),
+                button(type="Submit", value="Search", class_="btn btn-primary")(
+                    i(class_="fas fa-search")
+                ),
             )
+
         )
     )
-    return str(content)
+    return str(main_page)
 
 @app.route('/movies', methods=["POST"])
 def movies():
@@ -30,6 +45,7 @@ def movies():
     print(request.form)
 
     name = request.form['movie_title']
+    
 
     content = html(
         head(
--- src/config.py
+++ src/config.py
@@ -1,8 +1,15 @@
+# Title of the web app
+TITLE = "Movie Content Advisory Directory"
+
 # API Key can be generated from https://rapidapi.com/apidojo/api/imdb8
 API_KEY = "Enter API Key"
+
+# Favicon file
+FAVICON = "public/fav/favicon.ico"
 
 # Site logo
 LOGO = "/public/img/logo.png"
 
 # If movie has no available image it will use this
 NULL_POSTER = "/public/img/null_image.png"
+
