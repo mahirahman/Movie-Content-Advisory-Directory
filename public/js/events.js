075a2efd6eace4e11e6c619c69b155daeec4e40c
Mahi <starexify@gmail.com>
2024-03-17 02:56:26 +1100

    Remove scroll-to-top on desktop

--- public/js/events.js
+++ public/js/events.js
@@ -1,13 +1,13 @@
-window.addEventListener('scroll', function() {
-    var scrollButton = document.getElementById("scroll-to-top");
-    if (window.innerWidth <= 768 && window.scrollY > 100) {
-        scrollButton.style.display = "block";
-    } else {
-        scrollButton.style.display = "none";
-    }
-});
-
 if (window.location.href.includes('movies') || window.location.href.includes('advisory')) {
+    window.addEventListener('scroll', function() {
+        var scrollButton = document.getElementById("scroll-to-top");
+        if (window.innerWidth <= 768 && window.scrollY > 100) {
+            scrollButton.style.display = "block";
+        } else {
+            scrollButton.style.display = "none";
+        }
+    });
+
     document.getElementById("scroll-to-top").addEventListener("click", function() {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     });
